14f5d417f84df51a5ce3001d9f035b83c3497d62
Anku Choudhary <ankuchoudhary@Ankus-MacBook-Air.local>
2020-05-26 19:03:50 +0530

    Methods added

--- program.cs
+++ program.cs
@@ -1,1 +1,27 @@
 this is text
+
+using System;
+
+Class Solution 
+{
+    Static Void Main()
+    {
+        
+    }
+
+    public int multiplication(int x, int y)
+    {
+        return x*y;
+    }
+    
+    public int Add(int x, int y)
+    {
+        return x+y;
+    }
+    
+    public int Subtraction(int x, int y)
+    {
+        return x-y;
+    }
+
+}
